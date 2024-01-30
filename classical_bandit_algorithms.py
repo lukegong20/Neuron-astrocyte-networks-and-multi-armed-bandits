8013ff410d2d56952e31ef911485f5402e6f42fb
Luke Gong <80451579+lukegong20@users.noreply.github.com>
2024-01-30 12:57:06 -0600

    Update classical_bandit_algorithms.py

--- classical_bandit_algorithms.py
+++ classical_bandit_algorithms.py
@@ -1,5 +1,23 @@
-import torch
+import torch 
+import torch.nn as nn
+import torch.nn.functional as F
+import numpy as np
+import random
+import sys
+import math
 
+import os.path
+import os
+os.environ["CUBLAS_WORKSPACE_CONFIG"] = ":4096:8"
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+from torch.distributions.categorical import Categorical
+from torch.distributions.beta import Beta
+from tqdm import tqdm, trange
+from collections import defaultdict
+from torch.distributions.bernoulli import Bernoulli
+from torch.utils.data import Dataset, DataLoader, Subset
 
 class thompson_sampling():
 
