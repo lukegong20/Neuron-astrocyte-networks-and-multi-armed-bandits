fea454f6d9b94bd64d792d099640d9549b89fb73
Luke Gong <80451579+lukegong20@users.noreply.github.com>
2024-04-05 13:01:08 -0500

    Update RNN_bandit_algorthims.py

--- RNN_bandit_algorthims.py
+++ RNN_bandit_algorthims.py
@@ -12,13 +12,73 @@ from torch.distributions.beta import Beta
 
 
 
-class vanilla_RNN()
+""""vanilla_RNN""""
+class RNNnet(nn.Module):
+    def __init__(self, in_size, out_size, seed=None, *args, **kwargs):
+        super().__init__()
+        if seed is not None:
+            # use the provided seed value
+            self.seed = seed
+        else:
+            # use a random seed
+            self.seed = random.randint(0, 100)
+        self.seq = nn.RNN(in_size, hidden_size=128, num_layers=2,
+                                      batch_first=True)
+        self.fc = nn.Linear(128, out_size)
+        
+        
+
+    def forward(self, x, hidden=None):
+        
+        out, new_hidden = self.seq(x, hidden)
+        
+        return self.fc(out), new_hidden
 
 
-class GRU_RNN()
+""""GRU_RNN""""
+class GRUnet(nn.Module):
+    def __init__(self, in_size, out_size, seed=None, *args, **kwargs):
+        super().__init__()
+        if seed is not None:
+            # use the provided seed value
+            self.seed = seed
+        else:
+            # use a random seed
+            self.seed = random.randint(0, 100)
+        self.seq = nn.GRU(in_size, hidden_size=128, num_layers=2,
+                                      batch_first=True)
+        self.fc = nn.Linear(128, out_size)
+        
+        
+
+    def forward(self, x, hidden=None):
+        
+        out, new_hidden = self.seq(x, hidden)
+        
+        return self.fc(out), new_hidden
 
 
 
-class LSTM_RNN()
+"""LSTM_RNN"""
+class LSTMnet(nn.Module):
+    def __init__(self, in_size, out_size, seed=None, *args, **kwargs):
+        super().__init__()
+        if seed is not None:
+            # use the provided seed value
+            self.seed = seed
+        else:
+            # use a random seed
+            self.seed = random.randint(0, 100)
+        self.seq = nn.LSTM(in_size, hidden_size=128, num_layers=2,
+                                      batch_first=True)
+        self.fc = nn.Linear(128, out_size)
+        
+        
+
+    def forward(self, x, hidden=None):
+        
+        out, new_hidden = self.seq(x, hidden)
+        
+        return self.fc(out), new_hidden
 
 
